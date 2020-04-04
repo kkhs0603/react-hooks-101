d3cda1ea072b3ab310a004f814305d905f9751bf
kakke <pont3699@gmail.com>
2020-04-04 11:04:44 +0900

    React hook suseStateの練習

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  //１番目の要素は０番目の要素を操作する。
+  //price , setPrice 慣習
+  const [count, setCount] = useState(0)
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count -1)
+  const increment2 = () => setCount(previousCount => previousCount + 1)
+  const reset = () => setCount(0)
+  const double = () => setCount(previousCount => previousCount * 2)
+  const divide3 = () => setCount(previousCount => {
+    if(previousCount % 3 === 0){
+      return previousCount / 3 
+    }else{
+      return previousCount
+    }
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // <> <React.Fragment> は同義
+    <>
+      <div className="App">count: {count}</div>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+      <button onClick={increment2}>+1</button>
+      <div>
+        <button onClick={reset}>reset</button>
+        <button onClick={double}>x2</button>
+        <button onClick={divide3}>3で割れる時だけ商を出す</button>
+
+      </div>
+    </>
+  )
 }
 
 export default App;
