9a19dda3aef7893464e52e3761f004decb92b249
kakke <pont3699@gmail.com>
2020-04-10 00:14:13 +0900

    display events refact

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React ,{ useState, useReducer } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Event from './Event'
 import reducer from '../reducers'
 
 const App = ()=> {
@@ -41,10 +42,12 @@ const App = ()=> {
               <th>ID</th>
               <th>タイトル</th>
               <th>ボディー</th>
-              <th></th>
+              <th></th> 
             </tr>
           </thead>
-          <tbody></tbody>
+          <tbody>
+            {state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch}/>))}
+          </tbody>
         </table>
       </div>
     </>
